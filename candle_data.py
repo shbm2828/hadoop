573363464cf971eb396b0fea15a1c494eef4e6d5
shbm2828 <42641632+shbm2828@users.noreply.github.com>
2024-04-04 16:46:00 +0530

    Update candle_data.py file name with today

--- candle_data.py
+++ candle_data.py
@@ -2,23 +2,26 @@ import pandas as pd
 from threading import Thread
 import time
 import schedule
+from datetime import date
+
+today = str(date.today())
 
 class CE(Thread):
     def one_min_candle_data():
-        df = pd.read_csv('D:\\key\\april\\03_apr\\BN_CE_DATA.csv', header=None)
+        df = pd.read_csv('D:\\key\\april\\live_data\\BN_CE_DATA_'+today+'.csv', header=None)
         df.columns=['date', 'ltp']
         df['date'] = pd.to_datetime(df.date)  
         one_min_data = df.set_index('date').resample('1min').ohlc().reset_index()
-        one_min_data.to_csv(r'D:\\key\\april\\03_apr\\BN_CE_1min_candle.csv', header=None, index=False)
+        one_min_data.to_csv(r'D:\\key\\april\\candle_data\\BN_CE_1min_candle_'+today+'.csv', header=None, index=False)
         #time.sleep(60)
     
 
     def half_hr_candle_data():
-        df = pd.read_csv('D:\\key\\april\\03_apr\\BN_CE_DATA.csv', header=None)
+        df = pd.read_csv('D:\\key\\april\\live_data\\BN_CE_DATA_'+today+'.csv', header=None)
         df.columns=['date', 'ltp']
         df['date'] = pd.to_datetime(df.date)  
         half_hr_data = df.set_index('date').resample('30min', origin='start').ohlc().reset_index()
-        half_hr_data.to_csv(r'D:\\key\\april\\03_apr\\BN_CE_30min_candle.csv', header=None, index=False)
+        half_hr_data.to_csv(r'D:\\key\\april\\candle_data\\BN_CE_30min_candle_'+today+'.csv', header=None, index=False)
         #time.sleep(1800)
 
     schedule.every(1).minute.do(one_min_candle_data)
@@ -26,18 +29,18 @@ class CE(Thread):
  
 class PE(Thread):
     def one_min_candle_data():
-        df = pd.read_csv('D:\\key\\april\\03_apr\\BN_PE_DATA.csv', header=None)
+        df = pd.read_csv('D:\\key\\april\\live_data\\BN_PE_DATA_'+today+'.csv', header=None)
         df.columns=['date', 'ltp']
         df['date'] = pd.to_datetime(df.date)  
         one_min_data = df.set_index('date').resample('1min').ohlc().reset_index()
-        one_min_data.to_csv(r'D:\\key\\april\\03_apr\\BN_PE_1min_candle.csv', header=None, index=False)
+        one_min_data.to_csv(r'D:\\key\\april\\candle_data\\BN_PE_1min_candle_'+today+'.csv', header=None, index=False)
 
     def half_hr_candle_data():
-        df = pd.read_csv('D:\\key\\april\\03_apr\\BN_PE_DATA.csv', header=None)
+        df = pd.read_csv('D:\\key\\april\\live_data\\BN_PE_DATA_'+today+'.csv', header=None)
         df.columns=['date', 'ltp']
         df['date'] = pd.to_datetime(df.date)  
         half_hr_data = df.set_index('date').resample('30min', origin='start').ohlc().reset_index()
-        half_hr_data.to_csv(r'D:\\key\\april\\03_apr\\BN_PE_30min_candle.csv', header=None, index=False)
+        half_hr_data.to_csv(r'D:\\key\\april\\BN_PE_30min_candle_'+today+'.csv', header=None, index=False)
   
  
     schedule.every(1).minute.do(one_min_candle_data)
